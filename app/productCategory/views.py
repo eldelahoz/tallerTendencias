41d68fca056c1f387a3fa176508bca106a69e8f0
mateocar <mateocar96@gmail.com>
2024-09-07 11:27:13 -0500

    ADD: se crea el modelo productCategory

--- app/productCategory/views.py
+++ app/productCategory/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filter.rest_framework import DjangoFilterBackend
+from .models import *
+from .serealizer import *
 
-# Create your views here.
+class productCategoryViewset(viewsets.ModelViewSet):
+    queryset = product_category.objects(all)
+    serializer_class = productCategorySerializer
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+    filterset_fields = ('__all__')
+    search_fields = ('__all__')
+    ordering_fields = ('__all__')
+
+
+
+
